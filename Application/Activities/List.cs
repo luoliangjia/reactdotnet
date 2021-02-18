d354bfff7b9610bd9a5784d708f85338d227538a
luoliangjia <Luoliangjia@gmail.com>
2021-02-18 17:28:32 +1100

    section14

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -13,20 +15,24 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> { }
+        public class Query : IRequest<Result<List<ActivityDto>>> { }
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>>
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
         {
             private readonly DataContext _context;
             private readonly ILogger<List> _logger;
-            public Handler(DataContext context, ILogger<List> logger)
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, ILogger<List> logger, IMapper mapper)
             {
+                _mapper = mapper;
                 _logger = logger;
                 _context = context;
             }
 
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
+                // pass cancell token
+                
                 // try
                 // {
                 //     for (var i = 0; i < 10; i++)
@@ -40,7 +46,25 @@ namespace Application.Activities
                 // {
                 //     _logger.LogInformation("Task as cancelled");
                 // }
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
+                // return Result<List<Activity>>.Success(await _context.Activities.ToListAsync(cancellationToken));
+
+                
+                // using auto mapper, but end up SQL select other unwanted fields
+
+                // var activities = await _context.Activities
+                //     .Include(a => a.Attendees)
+                //     .ThenInclude(u => u.AppUser)
+                //     .ToListAsync();
+
+                // var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);    
+
+                // return Result<List<ActivityDto>>.Success(activitiesToReturn);
+
+                var activities = await _context.Activities
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync();
+
+                return Result<List<ActivityDto>>.Success(activities);                
 
             }
         }
