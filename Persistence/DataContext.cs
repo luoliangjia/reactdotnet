4a493c237663c7876d4674a905e17e66f55ed941
luoliangjia <Luoliangjia@gmail.com>
2021-02-23 16:38:04 +1100

    section19

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,6 +13,7 @@ namespace Persistence
         public DbSet<Activity> Activities {get; set;}
         public DbSet<ActivityAttendee> ActivityAttendees {get; set;}
         public DbSet<Photo> Photos {get; set;}
+        public DbSet<Comment> Comments {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -28,7 +29,12 @@ namespace Persistence
             builder.Entity<ActivityAttendee>()
                 .HasOne(a => a.Activity)
                 .WithMany(u => u.Attendees)
-                .HasForeignKey(aa => aa.ActivityId);                
+                .HasForeignKey(aa => aa.ActivityId);       
+
+            builder.Entity<Comment>()
+                .HasOne(a => a.Activity)        
+                .WithMany(c => c.Comments)
+                .OnDelete(DeleteBehavior.Cascade);            
         }
     }
 }
